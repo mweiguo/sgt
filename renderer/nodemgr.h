5c5cde1697b0f158cfb5ea70a7ef5adaa5bb2e98
mweiguo <mweiguo@gmail.com>
2010-08-08 16:34:56 +0000

    add deleteNode interface to NodeMgr

--- renderer/nodemgr.h
+++ renderer/nodemgr.h
@@ -6,46 +6,6 @@
 #include "sgnode.h"
 using namespace std;
 
-// class NodesVector : public vector<SGNode*>
-// {
-// public:
-//     static NodesVector& getInst()
-//     {
-//         static NodesVector inst;
-//         return inst;
-//     }
-// private:
-//     NodesVector() {}
-// };
-//
-// template < class T >
-// class NodeMgr : public std::map<int, T*>
-// {
-// public:
-//     static NodeMgr<T>& getInst () 
-//     {
-//         static NodeMgr<T> inst;
-//         return inst;
-//     }
-//     int addNode ()
-//     {
-//         int id = NodesVector::getInst().size();
-//         (*this)[id] = _lastNode = new T();
-//         NodesVector::getInst().push_back ( _lastNode );
-//         return id;
-//     }
-//     int addNode ( const T& node )
-//     {
-//         int id = NodesVector::getInst().size();
-//         (*this)[id] = _lastNode = new T(node);
-//         NodesVector::getInst().push_back ( _lastNode );
-//         return id;
-//     }
-//     T* lastNode () { return _lastNode; }
-// private:
-//     NodeMgr () {}
-//     T* _lastNode;
-// };
 class AGEF_EXPORT SeedGenerator
 {
 public:
@@ -110,6 +70,19 @@ public:
             return NULL;
         T* p = dynamic_cast<T*>(pp->second);
         return p;
+    }
+    
+    void deleteNode ( int id )
+    {
+	iterator pp = find ( id );
+	if ( pp != end() )
+	{
+	    SGNode* node = pp->second;
+	    node->removeAllChild ();
+	    node->setParentNode ( NULL );
+	    delete node;
+	    NodeMgr::getInst().erase ( id );
+	}
     }
 
     // this id is for respond to interface.h 's id
